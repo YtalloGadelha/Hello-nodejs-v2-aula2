2f629f983cfe9f465b6b253509bd6f35ddbed7fa
Ytallo de Lima Gadelha <ytallolg@gmail.com>
2016-11-17 11:44:57 -0300

    Ádição do arquvio hello3.js(Ler a partir do prompt)

---
+++ hello3.js
@@ -0,0 +1,18 @@
+//hello3.js
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+rl.question('Digite um numero\n', (num) =>{
+
+	if(isNaN(num))
+		console.log(`${num} não é um número válido!`);
+	
+	else
+		console.log(`Você digitou o número ${num}!`);
+	rl.close();
+});
