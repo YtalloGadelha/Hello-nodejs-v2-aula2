1f49f697ddce5444dfbeb2df781815cea7eadcca
Ytallo de Lima Gadelha <ytallolg@gmail.com>
2016-11-19 14:06:35 -0300

    Adição dos arquivos matematica.js(módulo matemática) e hello6.js(exemplo de uso do módulo matemática)

---
+++ hello6.js
@@ -0,0 +1,9 @@
+//hello6.js
+
+var libm = require("./matematica");
+
+var soma = libm.soma(2,2);
+var sub = libm.subtracao(4,2);
+
+console.log("O resultado da soma é: %d", soma);
+console.log("O resultado da subtração é: %d", sub);
---
+++ matematica.js
@@ -0,0 +1,10 @@
+//matematica.js
+
+const mat = {
+
+	soma: (a,b) =>  a + b,
+	subtracao: (a,b) => a - b
+};
+
+//aqui estamo publicando o conteúdo(ou parte do conteúdo) deste módulo
+module.exports = mat;
